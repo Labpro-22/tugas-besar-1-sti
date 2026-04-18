e58529fcf93fb3db7ebe973f98911830243cce5b
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2026-04-18 13:04:37 +0700

    feat(injector) : ITile

--- include/models/injector/injector.hpp
+++ include/models/injector/injector.hpp
@@ -1,4 +1,5 @@
 #pragma once
+class Player;
 
 class IDice {
     public:
@@ -6,4 +7,10 @@ class IDice {
         virtual int roll() = 0;
         virtual int rollSettingan(int die1, int die2) = 0;
         virtual bool isDouble() = 0;
+};
+
+class ITile {
+    public:
+        virtual ~ITile() {};
+        virtual void onLand(Player* player) = 0;
 };
