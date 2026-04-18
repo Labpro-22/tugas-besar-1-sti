0e499e0a3ba3d7fbe1ecdb871eca85c008bf7e5d
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2026-04-18 20:04:39 +0700

    feat(io): ConfigReader header

---
+++ include/models/io/ConfigReader.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+#include "../board/Board.hpp"
+
+// develop guidance :
+
+class Reader
+{
+    private:
+        std::string folderName;
+        std::string propertyConfigFileName;
+        std::string railRoadConfigFileName;
+        std::string utilityConfigFileName;
+        std::string taxConfigFileName;
+        std::string specialTileConfigFileName;
+        std::string othersConfigFileName;
+
+        // baca property
+        Board readProperty(); // board
+
+        // set map static yang jadi attribut kelas railroad
+        void readRailRoad();
+
+        // set map static yang jadi attribut kelas util
+        void readUtility();
+        
+        // pake set static aja
+
+        void readTax();
+
+        void readSpecial();
+
+        void readMisc();
+
+    public:
+        /*Buka folder sekaligus isi itu filename masing"*/
+        Reader(std::string folderName);
+
+        // load config jadi
+        ~Reader();
+};
+
