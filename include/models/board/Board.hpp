43eb48f15720cc00353c8f317de62f79d6e4aab9
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2026-04-21 04:04:29 +0700

    refactor: remove ITile class usage

--- include/models/board/Board.hpp
+++ include/models/board/Board.hpp
@@ -1,18 +1,19 @@
 #pragma once
-#include "../injector/injector.hpp"
+#include "models/tile/Tile.hpp"
 #include <vector>
 class Board
 {
     private:
         // perlu simpan size ga :/
-        std::vector<ITile*> tiles_;
+        std::vector<Tile&> tiles_;
     public:
         Board();
         ~Board();
-        void addTile(ITile* tile);
+        void addTile(Tile& tile);
         
         // geser ke tile setelah ditambahin distance (hasil roll dadu)
-        ITile* moveToNextTile(int distance);
+        Tile& moveToNextTile(int distance);
+        Tile& getCurrentTile(int id);
 
         int getJailPosition();
 };
