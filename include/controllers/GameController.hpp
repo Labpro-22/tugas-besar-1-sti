256e082d652858da0d3bd67e96b501dba1a6fd9e
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2026-04-22 13:51:11 +0700

    feat: init methods in  controllers

--- include/controllers/GameController.hpp
+++ include/controllers/GameController.hpp
@@ -1,12 +1,12 @@
 #include "models/board/Board.hpp"
-#include "models/injector/injector.hpp"
 #include "models/player/Player.hpp"
 #include "views/CommandInterface.hpp"
 #include "views/GameViewInterface.hpp"
-#include "models/injector/injector.hpp"
+#include "models/dice/Dice.hpp"
 #include "models/tile/action_tile/special_tile/PrisonTile.hpp"
 #include <memory>
 #include <vector>
+
 class GameController {
     public:
         enum class GameState  {
@@ -19,21 +19,31 @@ class GameController {
         };
 
         GameController(std::vector<std::unique_ptr<Player>>& players, 
-            Board& board, IDice& dice, GameViewInterface& view, 
+            Board& board, Dice& dice, GameViewInterface& view, 
             CommandInterface& command);
         ~GameController();
 
         void playGame(int latesTurn, int maxTurn);
         void processTurn(Player& p);
-        void normalTurn(Player& p, int firstDisplacement);
-        void auction(Player& p);
+        void processMovement(Player& p, int firstDisplacement);
+        void processSpecialCardUse(Player& p);
+        void processPickAndDropSpecialCard(Player& p);
+        void processRollDice(Player& p);
+        void processAuction(Player& p, PropertyTile& propertyTile);
+        void processMortgage();
+        void processBuyBuilding();
+        void processRedeem();
+        void processJailTurn(Player& p);
+        void processNormalTurn(Player& p);
 
-        void handleRollDice(Player& p);
+        void processTakeChanceCard(Player& p);
+        void processTakeCommunityChest(Player& p);
         bool hasSoleWinner() const;
 
     private:
+        // Deck<SpecialCard>...
         Board& board_;
-        IDice& dice_;
+        Dice& dice_;
         GameViewInterface& view_;
         CommandInterface& command_;
         std::vector<std::unique_ptr<Player>>& players_; // gabs pake &
--- src/controllers/GameController.cpp
+++ src/controllers/GameController.cpp
@@ -1,9 +1,9 @@
 #include "controllers/GameController.hpp"
 
 GameController::GameController(std::vector<std::unique_ptr<Player>>& players,
-    Board& board, IDice& dice, GameViewInterface& view, 
+    Board& board, Dice& dice, GameViewInterface& view,
     CommandInterface& command)
-    : players_(players), board_(board), dice_(dice),  
+    : players_(players), board_(board), dice_(dice),
     view_(view), command_(command) {}
 
 GameController::~GameController() = default;
@@ -32,85 +32,205 @@ bool GameController::hasSoleWinner() const{
     return countNotBankrupt == players_.size() - 1;
 }
 
+// RESTRIKSI : di luar on land / effect kartu aja la baru bs bolak balik:
 void GameController::processTurn(Player& p) {
-    Tile& currentTile = board_.getCurrentTile(p.getPosition());
+    view_.showMessage("Turn " + p.getUsername() + " dimulai!\n");
 
-    view_.kasihmsg("Turn player saat ini dimulai!\n");
+    // Pick & Drop Card Kemampuan
+    processPickAndDropSpecialCard(p);
 
-    int displacement = 0;
-    bool hasNotRollDice = true;
-
-    // Kasus lagi di-jail
-    if (p.isInJail()) {
-        // harus coba roll dice dulu
-        PrisonTile* prison = dynamic_cast<PrisonTile*>(&currentTile);
-        if (p.thisTurnAutoFreeFromJail()) {
-            view_.kasihmsg("Selamat! Player otomatis bebas dari penjara saat ini dengan membayar X duit!\n");
-            p.deductMoney(prison->getFineCost());
-            hasNotRollDice = false;
-            
-        } else {
-            view_.kasihmsg("Kamu saat ini berada di penjara!\n Kamu hanya dapat melakukan rolldice\n");
-            if (command_.RollSettingan()) {
-                int die1 = command_.GetInt(); // angka 1
-                int die2 = command_.GetInt(); // angka 2
-                displacement = dice_.rollSettingan(die1, die2);
-            } else {
-                displacement = dice_.roll();
-            }
-
-            if (dice_.isDouble()) {
-                // baru bs keluar
-                Tile& nextTile = board_.moveToNextTile(p->move(displacement));
-                nextTile.onLand(p);
-                return;
-            }
-            p.incrementJailTurn();
-        }
+    if (p.isInJail() && !p.thisTurnAutoFreeFromJail()) {
+        processJailTurn(p);
+        return;
+    } else if (p.isInJail() && p.thisTurnAutoFreeFromJail()) {
+        // bayar denda
+        // ya cari lah itu biaya penjara sisanya kyk biasa aja
     }
-    if (hasNotRollDice) {
-        view_.kasihmsg("Saatnya memilih kartu!\n");
-        // kartu dulu
-        chooseSpecialCard(p);
-        
-        // roll dice
-        if (command_.RollSettingan()) {
-            int die1 = command_.GetInt(); // angka 1
-            int die2 = command_.GetInt(); // angka 2
-            displacement = dice_.rollSettingan(die1, die2);
-        } else {
-            displacement = dice_.roll();
-        }
-    }
+    processNormalTurn(p);
+}
 
-    normalTurn(p, displacement);
+void GameController::processPickAndDropSpecialCard(Player& p) {
+    // sesuaikan di spek
+    // SkillCard& newcard = deck.drawDeck();
+    view_.showMessage("Kamu mendapatkan 1 kartu acak baru!\nKartu yang di dapat adalah.." );
+    // masukkan ke inventory player
+    // tampilin juga daftarnya...
+    // command_->getInt(0, total kartu)
+
+    view_.showMessage("Kartu x telah dibuang ....");
+}
+
+void GameController::processSpecialCardUse(Player& p) {
+    view_.showMessage("Silahkan pilih kartu yang mau kamu pakai!\n");
+    // cek apakah kartu yang dipakai bisa atau ga bs
+
+    // Sselama di Penjara,
+    // pemain tidak dapat bergerak (termasuk pergerakan yang di-invoke 
+    // dari kartu seperti move dan teleport). Untuk kartu lainnya, kasus
+    // penggunaannya sama seperti biasa.
+}
+
+void GameController::processRollDice(Player& p) {
+    view_.showMessage("Silahkan roll dice kamu!\n");
+
 
 }
 
-void GameController::normalTurn(Player& p, int firstDisplacement) {
-    Tile& nextTile = board_.moveToNextTile(p.move(firstDisplacement));
-    nextTile.onLand(p);
-    if (dice_.isDouble()) {
-        command_.cmd("Selamat anda mendapatkan double! Silahkan lanjutkan giliran anda!\n");
-        p.incrementDoubleCount();
-    }
+void GameController::processNormalTurn(Player& p) {
+    bool hasRolledFirsTime = false;
+    bool canSave = true;
 
-    while (dice_.isDouble() && p.safeToGetMoreDouble()) {
-        int displacement = 0;
-
-        // roll dice lagi
-        if (command_.RollSettingan()) {
-            int die1 = command_.GetInt(); // angka 1
-            int die2 = command_.GetInt(); // angka 2
-            displacement = dice_.rollSettingan(die1, die2);
-        } else {
-            displacement = dice_.roll();
+    // Command
+    while (p.notViolatingDoubleRollCount() && !p.isBankrupt() && !p.isInJail()) {
+        // dia bisa tiba-tiba jadi di penjara artinya berhenti udh (baru masuk)
+        if (p.isInJail()) {
+            break;
         }
+        Command cmd = command_.getCommand();
 
-        nextTile = board_.moveToNextTile(p.move(displacement));
-        nextTile.onLand(p);
+        switch (cmd.getType()) {
+            case CommandType::CETAK_PAPAN:
+                view_.cetakPapan();
+                break;
+            case CommandType::LEMPAR_DADU:
+                hasRolledFirsTime = true;
 
-        // add double count
-        p.incrementDoubleCount();
+                break;
+            case CommandType::ATUR_DADU:
+                hasRolledFirsTime = true;
+                //
+                break;
+            case CommandType::CETAK_AKTA:
+                view_.cetakAkta();
+                break;
+            case CommandType::CETAK_PROPERTI:
+                view_.cetakProperti();
+                break;
+            case CommandType::GADAI:
+                processMortgage();
+                //
+                break;
+            case CommandType::TEBUS:
+                processRedeem();
+                //
+                break;
+            case CommandType::BANGUN:
+                processBuyBuilding();
+                break;
+            case CommandType::SIMPAN:
+                // simpan..
+                break;
+            case CommandType::MUAT:
+                // langsung pesan error sajah
+                break;
+            case CommandType::CETAK_LOG:
+                // cetak semua lognya
+                break;
+            case CommandType::GUNAKAN_KEMAMPUAN:
+                if (!hasRolledFirsTime) {
+                    processSpecialCardUse(p);
+                } else {
+                    view_.showMessage("Special card hanya dapat dipakai sebelum roll dice pertama kali");
+                }
+                break;
+            
+            default:
+                break;
+        }
+        canSave = false;
     }
+
+    // cek apakah dia violate double itu / dia bankrupt / emg udh masuk aja ke penjara
+    // make sure kalo dia violate double itu bakal lgsg masuk penjara
+}
+
+void GameController::processJailTurn(Player& p) {
+    // udah pasti either bayar / atur" dadud
+    view_.showMessage("Anda sedang berada di penjara!\n");
+    Command cmd = command_.getCommand();
+
+    switch (cmd.getType()) {
+        case CommandType::GUNAKAN_KEMAMPUAN:
+            // ingat restriksi ketika dia di dalam penjara
+            processSpecialCardUse(p);
+            break;
+        case CommandType::LEMPAR_DADU:
+            // lempar dadu trs kalo double baru keluar
+            break;
+        case CommandType::ATUR_DADU:
+            // set dadu trs kalo double baru keluar
+            break;
+        case CommandType::BAYAR_DENDA:
+            // bayar terus lgsg keluar ke normal turn
+            processNormalTurn(p);
+            break;
+        default:
+            break;
+    }
+}
+
+// Bisa di-consider perlu kelas sendiri "AuctionController" atau engga
+void GameController::processAuction(Player& p, PropertyTile& propertyTile) {
+    // Kumpulin siapa aja yang ikut lelang
+    // Lanjutin sesuai spek :)
+}
+
+void GameController::processMovement(Player& p, int firstDisplacement) {
+    Tile& nextTile = board_.moveToNextTile(p.move(firstDisplacement));
+
+    // proses di dalam land
+    OnLandResult result = nextTile.onLand(p, command_, view_);
+
+    switch (result) {
+        // PROBLEM : KALO orang lain bankrut karena Lasso card gimana dong
+        case OnLandResult::TriggerAuction: // Ketika ga beli properti saja
+            // Kasus kalo emang lagi di land property
+            // Pemain menolak membeli properti
+            // Pemain tidak mampu membeli
+
+            // Tapi kasus kalo bankrut gimana dong,
+            // bisa jadi ga lagi di propertnya dia
+            // bisa jadi malah bukan giliran orang bersangkutan
+            // karena lasso card
+
+            // processAuction(p, nextTile);
+            break;
+        case OnLandResult::TriggerBankruptcyAuction: //
+            // Pemain bangkrut ke Bank (semua properti dilelang)
+                // Penyebab bankrut :
+                // tidak mampu memenuhi kewajiban pembayaran
+                // baik berupa sewa, pajak, 
+                // maupun efek dari kartu tertentu
+            // posisinya lagi di player sekarang ga si hrsnya tapi si lasso card ni gmnn weeee
+        case OnLandResult::TakeChanceCard:
+            processTakeChanceCard(p);
+            break;
+        case OnLandResult::TakeCommunityChest:
+            processTakeCommunityChest(p);
+            break;
+        case OnLandResult::Done: // yang kelar
+            break;
+            // go to jail perlu?
+        default:
+            break;
+    }
+}
+
+void GameController::processTakeChanceCard(Player& p) {
+    // Ngambil kartu dari deck
+    // Kartu Kesempatan
+    // Berikut adalah aturan Kartu Kesempatan yang ada dalam Permainan Nimonspoli:
+    // "Pergi ke stasiun terdekat."
+    // "Mundur 3 petak."
+    // "Masuk Penjara."
+}
+
+void GameController::processTakeCommunityChest(Player& p) {
+    // Ngambill kartu dari deck
+    // pake effect dari kartunya
+    // "Ini adalah hari ulang tahun Anda. Dapatkan M100 dari setiap pemain."
+    // "Biaya dokter. Bayar M700."
+    // "Anda mau nyaleg. Bayar M200 kepada setiap pemain."
+
+    // CommunityChest parameternya hanya boleh pemain (yang punya) dan juga semua player
 }
