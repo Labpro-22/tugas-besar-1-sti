8cbf16e9cf995c5099b70f87ddf8098f597a63ac
agathatatiana936 <agatha2ndemail@gmail.com>
2026-04-26 08:47:29 +0700

    fix(prisonTile): add logic prisonTile

--- src/models/tile/action_tile/special_tile/PrisonTile.cpp
+++ src/models/tile/action_tile/special_tile/PrisonTile.cpp
@@ -6,6 +6,6 @@
 #include <stdexcept>
 
 OnLandResult PrisonTile::onLand(Player& p, CommandInterface& command, GameViewInterface& view) {
-    view.showMessage("Yahahaha ga ngapain\" lanjut jalan sana gih\n");
-    return OnLandResult::Done;
+    view.showMessage("Kamu mendarat di Penjara. Kamu ditangkap oleh polisi!\n");
+    return OnLandResult::TriggerMoveToJail;
 }
